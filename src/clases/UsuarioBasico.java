f3f967fab77baf1b328f85d0a786612f8b5b39f3
MarcoM0404 <marco.marrazzo04@gmail.com>
2025-10-13 16:46:37 -0300

    cambios

--- src/clases/UsuarioBasico.java
+++ src/clases/UsuarioBasico.java
@@ -1,26 +1,7 @@
 package clases;
 
 public class UsuarioBasico extends Usuario {
-	private String icbnPrestado; // si no tiene ningun prestamo, es null
-
 	public UsuarioBasico(String nombre, String apellido, String correo, String password, EstadoUsuario estado) {
 		super(nombre, apellido, correo, password, estado, TipoUsuario.USUARIO);
-		this.icbnPrestado = null;
-	}
-
-	public boolean tienePrestamoVigente() {
-		return icbnPrestado != null;
-	}
-
-	public String getIcbnPrestado() {
-		return icbnPrestado;
-	}
-
-	public void asignarPrestamo(String icbn) {
-		this.icbnPrestado = icbn;
-	}
-
-	public void limpiarPrestamo() {
-		this.icbnPrestado = null;
 	}
 }
